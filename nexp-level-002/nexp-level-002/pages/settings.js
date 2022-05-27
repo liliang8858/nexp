d9361d5ac8e8e5e14af9c5086e05cca050af198c
liliang8858 <liliang8858@126.com>
2022-05-27 09:28:02 +0800

    update

---
+++ nexp-level-002/nexp-level-002/pages/settings.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import Leftm from "../components/leftm";
+
+export default function Settings() {
+  return (
+    <>
+
+      <style jsx>{`
+        .main {
+          position: absolute;
+          width: 96%;
+          height: 96%;
+          left: 2%;
+          top: 2%;
+
+          background: #ffffff;
+          box-shadow: 60px 70px 80px rgba(28, 31, 46, 0.1);
+          border-radius: 24px;
+        }
+        
+      `}</style>
+      <div className="main">
+        <Leftm />
+      </div>
+    </>
+  );
+}
