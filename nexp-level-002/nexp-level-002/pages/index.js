be5aaff9712ae00bad2611d080ba0a581cedfe3d
liliang8858 <liliang8858@126.com>
2022-06-03 13:57:27 +0800

    update media

--- nexp-level-002/nexp-level-002/pages/index.js
+++ nexp-level-002/nexp-level-002/pages/index.js
@@ -1,27 +1,12 @@
 import Head from "next/head";
 import Leftm from "../components/leftm";
 import Body from "../components/body";
+import IndexStyle from "../components/index.module.css"
 
 export default function Home() {
   return (
     <>
-
-      <style jsx>{`
-        .main {
-          position: absolute;
-          width: 96%;
-          height: 96%;
-          left: 2%;
-          top: 2%;
-
-          background: #ffffff;
-          box-shadow: 60px 70px 80px rgba(28, 31, 46, 0.1);
-          border-radius: 24px;
-          display: flex;
-        }
-        
-      `}</style>
-      <div className="main">
+      <div className={IndexStyle.main}>
         <Leftm />
         <Body />
       </div>
